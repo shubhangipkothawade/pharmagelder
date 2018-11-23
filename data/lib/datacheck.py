e0cdc298832697d04e4acaa2b61589ea2c531e99
simonhuwiler <simon.huwiler@ringier.ch>
2018-11-23 15:26:52 +0100

    add rnd

--- data/lib/datacheck.py
+++ data/lib/datacheck.py
@@ -76,38 +76,15 @@ def check_dataframe_accumulations(ds):
                 print(column + " not a number")
 
     #Check types
-    types = ["hcp_amount", "hcp_count", "hco_amount", "hco_count"]
+    types = ["hcp_amount", "hcp_count", "hco_amount", "hco_count", "rnd"]
     for t in types:
         if len(ds[ds.type == t]) != 1:
             print(t + " not found or not unique")
-        
 
-    """
-    if not np.issubdtype(ds['donations_grants'].dtype, np.number):
-        print("donations_grants not a number")
-        
-    if not np.issubdtype(ds['sponsorship'].dtype, np.number):
-        print("sponsorship not a number")
-        
-    if not np.issubdtype(ds['registration_fees'].dtype, np.number):
-        print("registration_fees not a number")
-        
-    if not np.issubdtype(ds['travel_accommodation'].dtype, np.number):
-        print("travel_accommodation not a number")
-        
-    if not np.issubdtype(ds['fees'].dtype, np.number):
-        print("fees not a number")
-        
-    if not np.issubdtype(ds['related_expenses'].dtype, np.number):
-        print("related_expenses not a number")
-        
-    if not np.issubdtype(ds['total'].dtype, np.number):
-        print("total not a number")
-    """
-        
     #Check if Sum = total
     if np.issubdtype(ds['total'].dtype, np.number):
         ds['_total'] = ds[["donations_grants", "sponsorship", 'registration_fees', 'travel_accommodation', 'fees', 'related_expenses']].sum(axis=1, skipna=True)
+        ds.loc[ds.type == 'rnd', "_total"] = ds.loc[ds.type == 'rnd', "total"]
         if len(ds[ds.total.round(2) != ds._total.round(2)]) > 0:
             print("Total nicht Summe der Werte")
         ds = ds.drop(columns=['_total'], inplace=True)
