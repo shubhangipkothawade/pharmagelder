cea3d9fb2341ee020fc8e71cb035e75cf92deb7d
simonhuwiler <simon.huwiler@ringier.ch>
2019-01-22 14:49:53 +0100

    title fix

--- data/lib/consts.py
+++ data/lib/consts.py
@@ -3,4 +3,4 @@ number_fields = ["donations_grants", "sponsorship", "registration_fees", "travel
 column_export_information = '_export_information'
 fix_columns_accumulations = ['type', 'donations_grants', 'sponsorship', 'registration_fees','travel_accommodation', 'fees', 'related_expenses', 'total', 'source']
 
-regex_title = r'\b(Dr|med|prof|prakt|pd|dipl|Arzt|Mr|Ms|Mrs|Docteur)[\s[.]]*'
+regex_title = r'\b(Dr|med|prof|phil|pharm|prakt|pd|dipl|Arzt|Mr|Ms|Mrs|Docteur)[\s[.]]*'
