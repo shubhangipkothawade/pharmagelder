7b7b8ae4ae0a72fe56fca7a6b02e8413d8156974
simonhuwiler <simon.huwiler@ringier.ch>
2019-01-25 16:32:52 +0100

    update matching

--- tasks/5. matching/0_match_hcp.py
+++ tasks/5. matching/0_match_hcp.py
@@ -7,56 +7,104 @@ import pandas as pd
 import numpy as np
 from fuzzywuzzy import fuzz
 import time
+import sys
+import os.path
+
+
+#%%
+version = 0.1
+
+#%% [markdown]
+# ## Check path
+# Check, if we run in in the git directory or on the server. If on a server, look for the files in the same directory
+
+#%%
+#Check Server or Git
+path_git = '../../data/3. transformation/2_list_expanded.csv'
+path_server = '2_list_expanded.csv'
+
+on_git = os.path.isfile(path_git)
 
 #%% [markdown]
 # ## Read Data
 
 #%%
-df = pd.read_csv('2. list_expanded.csv')
-#df = pd.read_csv('../../data/3. transformation/2. list_expanded.csv')
-df_hcp = df[df.type == 'hcp'].copy()
+if on_git:
+    df = pd.read_csv(path_git)
+else:
+    df = pd.read_csv(path_server)
+
+df_data = df[df.type == 'hcp'].copy()
+df_data['address'] = df_data['address'].fillna("")
+
+#For Testing
+df_data = df_data[df_data.source.isin(['eli', 'shire', 'almirall'])]
 
 #Reset index
-df_hcp = df_hcp.reset_index(drop=True)
+df_data = df_data.reset_index(drop=True)
 
 #Set Startindex to 1
-df_hcp.index += 1 
-len(df_hcp)
+df_data.index += 1
+
+total_rows = len(df_data)
 
 #%% [markdown]
 # ## Calc rows
 
 #%%
 #Add Parent
-df_hcp['parent'] = 0
-df_hcp['parent'] = df_hcp['parent'].astype(int)
+df_data['parent'] = 0
+df_data['parent'] = df_data['parent'].astype(int)
+df_data['parent'] = df_data.index
 
 #Convert
-df_hcp['name'] = df_hcp['name'].astype("str")
-df_hcp['address_expand'] = df_hcp['address_expand'].astype("str")
-df_hcp['location_expand'] = df_hcp['location_expand'].astype("str")
+df_data['name'] = df_data['name'].astype("str")
+df_data['address_expand'] = df_data['address_expand'].astype("str")
+df_data['location_expand'] = df_data['location_expand'].astype("str")
+
+#Sort
+df_data = df_data.sort_values('name')
 
 start_time = time.time()
 
-for index, row in df_hcp.iterrows():
-    if index % 10 == 0:
-        print(index)
+print("===============================")
+print("Start fuzzy matcher HCP %s" % version)
+print("Rows to match: %s" % total_rows)
+print("===============================")
+
+counter = 0
+for index, row in df_data.iterrows():
     
-    #Calculate ratio for each category
-    df_hcp['r_name'] = df_hcp['name'].apply(lambda x: fuzz.token_set_ratio(x.lower(), row['name'].lower()))
-    df_hcp['r_location'] = df_hcp['location_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['location_expand']))
-    df_hcp['r_address'] = df_hcp['address_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['address_expand']))
+    if counter % 10 == 0:
+        sys.stdout.write("\rProgress: %s%%" % round(100 / total_rows * counter, 2))
+        sys.stdout.flush()
+        
+    df_data['r_name'] = df_data['name'].apply(lambda x: fuzz.token_set_ratio(x.lower(), row['name'].lower()))
+    df_data['r_location'] = df_data['location_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['location_expand']))
+    df_data['r_address'] = df_data['address_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['address_expand']))
+    df_data['r_ratio'] = df_data['r_name'] + df_data['r_location'] + df_data['r_address']
     
-    #Set the rules.
-    df_hcp.loc[(df_hcp.r_name >= 80) & (df_hcp.r_location >= 85) & (df_hcp.r_address >= 80), 'parent'] = index
+    condition_fix = (df_data.index != index) & (df_data['parent'] != index)
+    if row['address'] == '':
+        condition1 = (df_data.r_name >= 80) & (df_data.r_location >= 85) & (condition_fix)
+    else:
+        condition1 = (df_data.r_name >= 80) & (df_data.r_location >= 85) & (df_data.r_address >= 75) & (condition_fix)
     
+    highest_match = df_data[(condition1)].nlargest(1, columns=['r_ratio'])
+    if len(highest_match) == 1:
+        df_data.loc[index, 'parent'] = highest_match.iloc[0].name
+        df_data.loc[index, 'log'] = "name=%s,location=%s,address=%s,total=%s" % (highest_match.iloc[0].r_name, highest_match.iloc[0].r_location, highest_match.iloc[0].r_address, highest_match.iloc[0].r_ratio)
     
+    counter += 1
+
 elapsed_time = time.time() - start_time
-print('Finished in: ' + str(round(elapsed_time / 60, 2)) + ' minutes')
+print('\nFinished in: ' + str(round(elapsed_time / 60, 2)) + ' minutes')
 
 
 #%%
-df_hcp.to_csv('3. hcp_matches.csv', index=True)
-#df_hcp.to_csv('../../data/3. transformation/3. hcp_matches.csv', index=True)
+if on_git:
+    df_data.to_csv('../../data/3. transformation/3_hcp_matches.csv', index=True)
+else:
+    df_data.to_csv('3_hcp_matches.csv', index=True)
 
 
--- tasks/5. matching/1_match_hco.py
+++ tasks/5. matching/1_match_hco.py
@@ -1,4 +1,3 @@
-#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataSciece.changeDirOnImportExport setting
 #%% [markdown]
 # # 1. Match HCOs
 # The magic! Here we match addresses. You can run this file on an external server - it will take some times
@@ -8,88 +7,120 @@ import pandas as pd
 import numpy as np
 from fuzzywuzzy import fuzz
 import time
+import sys
+import os.path
+
+
+#%%
+version = 0.1
+
+#%% [markdown]
+# ## Check path
+# Check, if we run in in the git directory or on the server. If on a server, look for the files in the same directory
+
+#%%
+#Check Server or Git
+path_git = '../../data/3. transformation/2_list_expanded.csv'
+path_server = '2_list_expanded.csv'
+
+on_git = os.path.isfile(path_git)
 
 #%% [markdown]
 # ## Read Data
 
 #%%
-#f = pd.read_csv('2. list_expanded.csv')
-d#f = pd.read_csv('../../data/3. transformation/2_list_expanded.csv')
-df_hcp = df[df.type == 'hco'].copy()
-df_hcp['address'] = df_hcp['address'].fillna("")
+if on_git:
+    df = pd.read_csv(path_git)
+else:
+    df = pd.read_csv(path_server)
+
+df_data = df[df.type == 'hco'].copy()
+df_data['address'] = df_data['address'].fillna("")
 
 #For Testing
-#df_hcp = df_hcp[df_hcp.source.isin(['eli', 'shire', 'almirall'])]
+df_data = df_data[df_data.source.isin(['eli', 'shire', 'almirall'])]
 
 #Reset index
-df_hcp = df_hcp.reset_index(drop=True)
+df_data = df_data.reset_index(drop=True)
 
 #Set Startindex to 1
-df_hcp.index += 1
-len(df_hcp)
+df_data.index += 1
+
+total_rows = len(df_data)
 
 #%% [markdown]
 # ## Calc rows
 
 #%%
 #Add Parent
-df_hcp['parent'] = 0
-df_hcp['parent'] = df_hcp['parent'].astype(int)
-df_hcp['parent'] = df_hcp.index
+df_data['parent'] = 0
+df_data['parent'] = df_data['parent'].astype(int)
+df_data['parent'] = df_data.index
 
 #Convert
-df_hcp['name'] = df_hcp['name'].astype("str")
-df_hcp['address_expand'] = df_hcp['address_expand'].astype("str")
-df_hcp['location_expand'] = df_hcp['location_expand'].astype("str")
-
-df_hcp['concat'] = df_hcp['name'] + ' ' + df_hcp['location_expand']
+df_data['name'] = df_data['name'].astype("str")
+df_data['address_expand'] = df_data['address_expand'].astype("str")
+df_data['location_expand'] = df_data['location_expand'].astype("str")
 
 #Sort
-df_hcp = df_hcp.sort_values('name')
-
+df_data = df_data.sort_values('name')
 
 start_time = time.time()
 
+print("====================")
+print("Start fuzzy matcher %s" % version)
+print("Rows to match: %s" % total_rows)
+print("====================")
+
 counter = 0
-for index, row in df_hcp.iterrows():
+for index, row in df_data.iterrows():
     
     if counter % 10 == 0:
-        print(counter)
-    #print(counter)
+        sys.stdout.write("\rProgress: %s%%" % round(100 / total_rows * counter, 2))
+        sys.stdout.flush()
 
     """
     #Works!
-    df_hcp['r_name'] = df_hcp['name'].apply(lambda x: fuzz.token_set_ratio(x.lower(), row['name'].lower()))
-    df_hcp['r_location'] = df_hcp['location_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['location_expand']))
-    df_hcp['r_address'] = df_hcp['address_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['address_expand']))
-    df_hcp['r_ratio'] = df_hcp['r_name'] + df_hcp['r_location']# + df_hcp['r_address']
+    df_data['r_name'] = df_data['name'].apply(lambda x: fuzz.token_set_ratio(x.lower(), row['name'].lower()))
+    df_data['r_location'] = df_data['location_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['location_expand']))
+    df_data['r_address'] = df_data['address_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['address_expand']))
+    df_data['r_ratio'] = df_data['r_name'] + df_data['r_location']# + df_data['r_address']
     
-    highest_match = df_hcp[(df_hcp.r_name > 80) & (df_hcp.r_location > 85) & (df_hcp.index != index)].nlargest(1, columns=['r_ratio'])
+    highest_match = df_data[(df_data.r_name > 80) & (df_data.r_location > 85) & (df_data.index != index)].nlargest(1, columns=['r_ratio'])
     if len(highest_match) == 1:
-        df_hcp.loc[index, 'parent'] = highest_match.iloc[0].name
+        df_data.loc[index, 'parent'] = highest_match.iloc[0].name
     """
         
-        
-    df_hcp['r_name'] = df_hcp['name'].apply(lambda x: fuzz.token_set_ratio(x.lower(), row['name'].lower()))
-    df_hcp['r_location'] = df_hcp['location_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['location_expand']))
-    df_hcp['r_address'] = df_hcp['address_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['address_expand']))
-    df_hcp['r_ratio'] = df_hcp['r_name'] + df_hcp['r_location'] + df_hcp['r_address']
+    df_data['r_name'] = df_data['name'].apply(lambda x: fuzz.token_set_ratio(x.lower(), row['name'].lower()))
+    df_data['r_location'] = df_data['location_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['location_expand']))
+    df_data['r_address'] = df_data['address_expand'].apply(lambda x: fuzz.token_set_ratio(x, row['address_expand']))
+    df_data['r_ratio'] = df_data['r_name'] + df_data['r_location'] + df_data['r_address']
     
-    condition_fix = (df_hcp.index != index) & (df_hcp['parent'] != index)
-    condition1 = (df_hcp.r_ratio > 240) & (df_hcp.r_location > 85) & (condition_fix)
-    condition2 = (df_hcp.r_name > 80) & (df_hcp.r_location > 85) & (condition_fix)
-    highest_match = df_hcp[(condition1) | (condition2)].nlargest(1, columns=['r_ratio'])
+    condition_fix = (df_data.index != index) & (df_data['parent'] != index)
+    if row['address'] == '':
+        condition1 = (df_data.r_name >= 80) & (df_data.r_location >= 85) & (condition_fix)
+    else:
+        condition1 = (df_data.r_name >= 80) & (df_data.r_location >= 85) & (df_data.r_address >= 75) & (condition_fix)
+    
+    #condition1 = (df_data.r_name >= 75) & (df_data.r_location >= 85) & (condition_fix)
+    #condition2 = (df_data.r_ratio >= 240) & (df_data.r_location >= 85) & (condition_fix)
+    highest_match = df_data[(condition1)].nlargest(1, columns=['r_ratio'])
+    #highest_match = df_data[(condition1) | (condition2)].nlargest(1, columns=['r_ratio'])
     if len(highest_match) == 1:
-        df_hcp.loc[index, 'parent'] = highest_match.iloc[0].name
+        df_data.loc[index, 'parent'] = highest_match.iloc[0].name
+        df_data.loc[index, 'log'] = "name=%s,location=%s,address=%s,total=%s" % (highest_match.iloc[0].r_name, highest_match.iloc[0].r_location, highest_match.iloc[0].r_address, highest_match.iloc[0].r_ratio)
     
     counter += 1
+
 elapsed_time = time.time() - start_time
-print('Finished in: ' + str(round(elapsed_time / 60, 2)) + ' minutes')
+print('\nFinished in: ' + str(round(elapsed_time / 60, 2)) + ' minutes')
 
 
 #%%
-df_hcp.to_csv('3. hco_matches.csv', index=True)
-#df_hcp.to_csv('../../data/3. transformation/3_hco_matches.csv', index=True)
+if on_git:
+    df_data.to_csv('../../data/3. transformation/3_hco_matches.csv', index=True)
+else:
+    df_data.to_csv('3_hco_matches.csv', index=True)
 
 
 #%%
@@ -107,10 +138,10 @@ importlib.reload(pdfexport)
 from pdfexport import *
 
 
-df_index = df_hcp.set_index([df_hcp.parent, df_hcp.index])
+df_index = df_data.set_index([df_data.parent, df_data.index])
 df_index.head()
 
-write_to_excel(df_hcp, 'tmp.xlsx', open=True, index=True)
+write_to_excel(df_data, 'tmp.xlsx', open=True, index=True)
 """
 
 
