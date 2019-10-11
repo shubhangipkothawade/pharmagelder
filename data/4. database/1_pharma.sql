bfea83e940f27f783cfff55d220d91f7b8fa0088
Simon Huwiler <simon.huwiler@ringier.ch>
2019-10-11 16:56:49 +0200

    cleanup

--- data/4. database/1_pharma.sql
+++
@@ -1,5 +0,0 @@
-START TRANSACTION;
-INSERT INTO pharma (`pha_id`, `pha_name`) VALUES 
-('41', ' Nordic Pharma'), 
-('60', 'Recordati');
-COMMIT;
--- data/4. database/2_pharma_source.sql
+++
@@ -1,5 +0,0 @@
-START TRANSACTION;
-INSERT INTO pharma_source (`phs_fk_pharma`, `phs_source`) VALUES 
-('60', 'http://recordati.ch/de/home/transparenz-pharma-kooperations-kodex/'), 
-('41', 'https://www.nordicpharma.ch/about-us/transparency/');
-COMMIT;
