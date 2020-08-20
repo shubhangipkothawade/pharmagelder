77dcdabf3cd4bc100b51f9c1ec0ffba28551f6ec
nanoteilchen <simon.huwiler@ringier.ch>
2020-08-20 19:28:39 +0200

    stg test

--- data/4. database/data_dump.sql
+++ data/4. database/data_dump.sql
@@ -17695,6 +17695,10 @@ INSERT INTO accumulation (`acc_fk_pharma`, `acc_year`, `acc_fk_transaction_categ
 ('57.0', '2019', '7', '548960.62', 'rnd');
 COMMIT;
 START TRANSACTION;
-UPDATE transaction SET tra_fk_recipient = 10490 WHERE tra_fk_recipient = 10485;
-DELETE FROM recipient WHERE rec_id = 10485;
+ALTER TABLE pharma DROP IF EXISTS pha_note;
+ALTER TABLE pharma ADD pha_note TEXT NULL;
+UPDATE pharma SET pha_note = '{"de": "Seit 2019 weist Actelion Offenlegungen nicht mehr gesondert aus. Die Daten sind neu in den Veröffentlichungen der Firma Janssen-Cilag enthalten.", "fr": "XXXNOTEXXX"}' WHERE pha_id = 2;
+UPDATE pharma SET pha_note = '{"de": "Seit 2019 weist Alcon Offenlegungen nicht mehr gesondert aus. Die Daten sind neu in den Veröffentlichungen der Firma Novartis enthalten.", "fr": "XXXNOTEXXX"}' WHERE pha_id = 3;
+UPDATE pharma SET pha_note = '{"de": "Basilea hat den Pharma-Kooperations-Kodex nicht unterzeichnet, Veröffentlichungen geschehen auf freiwilliger Basis. Für 2019 hat das Unternehmen noch keine Zahlen veröffentlicht.", "fr": "XXXNOTEXXX"}' WHERE pha_id = 25;
+UPDATE pharma SET pha_note = '{"de": "Seit 2019 weist Teva Offenlegungen nicht mehr gesondert aus. Die Daten sind neu in den Veröffentlichungen der Firma Mepha enthalten.", "fr": "XXXNOTEXXX"}' WHERE pha_id = 55;
 COMMIT;
