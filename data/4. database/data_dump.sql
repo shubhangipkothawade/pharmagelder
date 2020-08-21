df3a8dd01110593736df5bf493dd3b19c560f8d7
simonhuwiler <webmaster@simonhuwiler.ch>
2020-08-21 17:49:13 +0200

    new run

--- data/4. database/data_dump.sql
+++ data/4. database/data_dump.sql
@@ -6730,8 +6730,8 @@ INSERT INTO transaction (`tra_fk_pharma`, `tra_fk_recipient`, `tra_year`, `tra_f
 ('15', '4284', '2019', '4', '456.13', 'Karl Peter Bolliger', 'Altendorf', 'Dorfplatz 2'), 
 ('57', '13569', '2019', '3', '350.08', 'Karl Renggli', 'Gampelen', 'Mediteam Gampelen AG, Insstrasse 9'), 
 ('57', '13569', '2019', '4', '688.36', 'Karl Renggli', 'Gampelen', 'Mediteam Gampelen AG, Insstrasse 9'), 
-('24', '13570', '2019', '3', '47265.0', 'Karl Schütz', 'Luzern', 'Zürichstrasse 7'), 
-('24', '13570', '2019', '4', '103930.0', 'Karl Schütz', 'Luzern', 'Zürichstrasse 7'), 
+('24', '13570', '2019', '3', '472.65', 'Karl Schütz', 'Luzern', 'Zürichstrasse 7'), 
+('24', '13570', '2019', '4', '1039.3', 'Karl Schütz', 'Luzern', 'Zürichstrasse 7'), 
 ('40', '4279', '2019', '5', '850.0', 'Karl Steuer', 'Allschwil', 'Radio-Onkologie Amsler AG, Brennerstrasse 12'), 
 ('35', '4290', '2019', '3', '323.0', 'Karl-Theobald Vinzent', 'Appenzell', 'Rütistrasse'), 
 ('35', '4290', '2019', '4', '434.0', 'Karl-Theobald Vinzent', 'Appenzell', 'Rütistrasse'), 
@@ -17697,9 +17697,9 @@ COMMIT;
 START TRANSACTION;
 ALTER TABLE pharma DROP IF EXISTS pha_note;
 ALTER TABLE pharma ADD pha_note TEXT NULL;
-UPDATE pharma SET pha_note = '{"de": "Seit 2019 weist Actelion Offenlegungen nicht mehr gesondert aus. Die Daten sind neu in den Veröffentlichungen der Firma Janssen-Cilag enthalten.", "fr": "Depuis 2019, les rapports de transparence d'Actelion ne sont plus publiés à part. Les données sont désormais intégrées dans les rapports de l'entreprise Janssen-Cilag."}' WHERE pha_id = 2;
-UPDATE pharma SET pha_note = '{"de": "Seit 2019 weist Alcon Offenlegungen nicht mehr gesondert aus. Die Daten sind neu in den Veröffentlichungen der Firma Novartis enthalten.", "fr": "Depuis 2019, les rapports de transparence d'Alcon ne sont plus publiés à part. Les données sont désormais intégrées dans les rapports de l'entreprise Novartis."}' WHERE pha_id = 3;
-UPDATE pharma SET pha_note = '{"de": "Basilea hat den Pharma-Kooperations-Kodex nicht unterzeichnet, Veröffentlichungen geschehen auf freiwilliger Basis. Für 2019 hat das Unternehmen noch keine Zahlen veröffentlicht.", "fr": "Basilea n'est pas signataire du Code de coopération pharmaceutique, ses publications de rapports se font sur une base volontaires. L'entreprise n'a pas communiqué de chiffres pour 2019."}' WHERE pha_id = 25;
-UPDATE pharma SET pha_note = '{"de": "Seit 2019 weist Teva Offenlegungen nicht mehr gesondert aus. Die Daten sind neu in den Veröffentlichungen der Firma Mepha enthalten.", "fr": "Depuis 2019, les rapports de transparence de Teva ne sont plus publiés à part. Les données sont désormais intégrées dans les rapports de l'entreprise Mepha."}' WHERE pha_id = 55;
+UPDATE pharma SET pha_note = '{"de": "Seit 2019 weist Actelion Offenlegungen nicht mehr gesondert aus. Die Daten sind neu in den Veröffentlichungen der Firma Janssen-Cilag enthalten.", "fr": "Depuis 2019, les rapports de transparence d\'Actelion ne sont plus publiés à part. Les données sont désormais intégrées dans les rapports de l\'entreprise Janssen-Cilag."}' WHERE pha_id = 2;
+UPDATE pharma SET pha_note = '{"de": "Seit 2019 weist Alcon Offenlegungen nicht mehr gesondert aus. Die Daten sind neu in den Veröffentlichungen der Firma Novartis enthalten.", "fr": "Depuis 2019, les rapports de transparence d\'Alcon ne sont plus publiés à part. Les données sont désormais intégrées dans les rapports de l\'entreprise Novartis."}' WHERE pha_id = 3;
+UPDATE pharma SET pha_note = '{"de": "Basilea hat den Pharma-Kooperations-Kodex nicht unterzeichnet, Veröffentlichungen geschehen auf freiwilliger Basis. Für 2019 hat das Unternehmen noch keine Zahlen veröffentlicht.", "fr": "Basilea n\'est pas signataire du Code de coopération pharmaceutique, ses publications de rapports se font sur une base volontaires. L\'entreprise n\'a pas communiqué de chiffres pour 2019."}' WHERE pha_id = 25;
+UPDATE pharma SET pha_note = '{"de": "Seit 2019 weist Teva Offenlegungen nicht mehr gesondert aus. Die Daten sind neu in den Veröffentlichungen der Firma Mepha enthalten.", "fr": "Depuis 2019, les rapports de transparence de Teva ne sont plus publiés à part. Les données sont désormais intégrées dans les rapports de l\'entreprise Mepha."}' WHERE pha_id = 55;
 UPDATE recipient SET rec_name = 'Novartis Pharma AG (als Empfänger)' WHERE rec_id = 10008;
 COMMIT;
