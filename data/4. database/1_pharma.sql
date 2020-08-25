610149571ac624f33af6f9cb9a3fef6afe33a312
nanoteilchen <simon.huwiler@ringier.ch>
2020-08-25 23:23:30 +0200

    cleanup

--- data/4. database/1_pharma.sql
+++
@@ -1,5 +0,0 @@
-START TRANSACTION;
-INSERT INTO pharma (`pha_id`, `pha_name`) VALUES 
-('61', 'Mitsubishi Tanabe Pharma GmbH'), 
-('62', 'Theramex');
-COMMIT;
--- data/4. database/2_pharma_source.sql
+++
@@ -1,5 +0,0 @@
-START TRANSACTION;
-INSERT INTO pharma_source (`phs_fk_pharma`, `phs_source`) VALUES 
-('61', 'https://www.mt-pharma-de.com/aktuelles/transparenz.html'), 
-('62', 'https://www.five.ch/futurehealth-declaration/');
-COMMIT;
